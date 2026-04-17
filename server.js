a1484c92401041a255949870ac1d7432baa947b9
nathakrit-rmutp <nathakrit-c@rmutp.ac.th>
2026-04-17 15:58:00 +0000

    update node

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors()); // 🔥 แก้ CORS
 app.use(express.json());
+app.use(express.static(path.join(__dirname))); // 🔥 Serve static files
 
 const GAS_URL = 'https://script.google.com/macros/s/AKfycbwq70-vF_Na5-71yyqGvfDKY6d3PwZhvgJ2dGjbTFF3RjzJHLA79XFqy3eESlZ2LJngrg/exec';
 
