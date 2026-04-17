fb17876d38fd8302797056dcdaa94aa2482f3b26
nathakrit-rmutp <nathakrit-c@rmutp.ac.th>
2026-04-17 15:56:16 +0000

    add server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors()); // 🔥 แก้ CORS
+app.use(express.json());
+
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwq70-vF_Na5-71yyqGvfDKY6d3PwZhvgJ2dGjbTFF3RjzJHLA79XFqy3eESlZ2LJngrg/exec';
+
+app.post('/api', async (req, res) => {
+    try {
+        const response = await fetch(GAS_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain;charset=utf-8'
+            },
+            body: JSON.stringify(req.body)
+        });
+
+        const text = await response.text();
+
+        res.send(text); // 🔥 ส่งกลับไป frontend
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ error: 'Server error' });
+    }
+});
+
+app.listen(3000, () => {
+    console.log('🚀 Proxy running on http://localhost:3000');
+});
