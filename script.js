595bc92cebbff5e45a8afd3139718b9ce980cb41
nathakrit-rmutp <nathakrit-c@rmutp.ac.th>
2026-04-18 15:23:22 +0000

    fix bug

--- script.js
+++ script.js
@@ -65,9 +65,11 @@ setInterval(() => {
 
 async function apiPost(body = {}) {
     try {
-        const res = await fetch('/api', {
+        const res = await fetch(API_URL, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {
+                'Content-Type': 'text/plain;charset=utf-8'
+            },
             body: JSON.stringify(body)
         });
         return await res.json();
